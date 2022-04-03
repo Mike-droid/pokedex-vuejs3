987dd365888a46944558b041023e64b7bb401b5c
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2022-04-03 14:06:44 -0500

    estilos

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="header">
     <label for="pokemonInput">
       Type pokemon name or ID:
       <input
@@ -9,36 +9,45 @@
       >
     </label>
     <button
+      class="searchButton"
       @click="searchPokemon"
     >
       Search pokemon!
     </button>
   </header>
 
-  <main v-if="Object.entries(pokemonData).length > 0">
-    <h1>{{ pokemonData.name }}</h1>
-    <img
-      :src="pokemonData.sprites.front_default"
-      :alt="pokemonData.name"
-    >
-    <ul class="type">
-      <li
-        v-for="(type, key) in pokemonData.types"
-        :key="key"
-        :class="type.type.name"
-      >
-        <span>{{ type.type.name }}</span>
-      </li>
-    </ul>
-    <ul class="stats">
-      <h2>Stats:</h2>
-      <li
-        v-for="(stat, key) in pokemonData.stats"
-        :key="key"
-      >
-        <span>{{ stat.stat.name}} -> {{stat.base_stat }}</span>
-      </li>
-    </ul>
+  <main
+    class="main"
+    v-if="Object.entries(pokemonData).length > 0"
+  >
+    <section class="pokemonCard">
+      <div class="nameImage">
+        <h1 class="pokemonName">{{ pokemonData.name }}</h1>
+        <img
+          :src="pokemonData.sprites.front_default"
+          :alt="pokemonData.name"
+        >
+      </div>
+      <ul class="type">
+        <h2>Type:</h2>
+        <li
+          v-for="(type, key) in pokemonData.types"
+          :key="key"
+          :class="type.type.name"
+        >
+          <span>{{ type.type.name }}</span>
+        </li>
+      </ul>
+      <ul class="stats">
+        <h2>Stats:</h2>
+        <li
+          v-for="(stat, key) in pokemonData.stats"
+          :key="key"
+        >
+          <span>{{ stat.stat.name}} -> {{stat.base_stat }}</span>
+        </li>
+      </ul>
+    </section>
   </main>
 </template>
 
@@ -74,8 +83,84 @@ export default {
 @import './pokemon_types.scss';
 @import url('https://fonts.googleapis.com/css2?family=Changa:wght@400;700&display=swap');
 
-header, main, input[type="text"], button {
+.header, .main, input[type="text"], .searchButton {
   font-family: 'Changa', sans-serif;
+}
+
+.header, input[type="text"], .searchButton {
+  font-size: 1.5rem;
+}
+
+.main {
+  font-size: 1.2rem;
+  background-color: $pokedex-green;
+}
+
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: $pokedex-red;
+  color: white;
+
+  & .searchButton {
+  background-color: #1cb02b;
+  color: white;
+  border: none;
+  margin-left: 10px;
+  border-radius: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #1c8b0a;
+  }
+}
+
+  & input[type="text"] {
+    border-radius: 10px;
+    outline: none;
+    border: none;
+  }
+}
+
+
+.pokemonCard {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: center;
+
+  & .nameImage, & .type, & .stats {
+    width: 33%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  & .nameImage {
+    & .pokemonName {
+      text-transform: capitalize;
+    }
+
+    & img {
+      width: 200px;
+      background-color: $pokedex-blue;
+      border-radius: 50%;
+    }
+  }
+
+  & .type li {
+    width: 90%;
+    margin-bottom: 10px;
+    text-align: center;
+    border-radius: 20px;
+  }
+
+  & .stats li {
+    align-self: flex-start;
+  }
 }
 
 ul {
@@ -92,8 +177,12 @@ ul {
 
 .stats {
   color: black;
-}
 
+  & li {
+    list-style: none;
+    text-transform: uppercase;
+  }
+}
 
 .normal {
   background-color: $normal
@@ -148,5 +237,50 @@ ul {
 }
 .fairy {
   background-color: $fairy
+}
+
+@media screen and (max-width: 820px) {
+
+  .header {
+    flex-direction: column;
+    height: 120px;
+
+    & .searchButton {
+      width: 70%;
+      margin-top: 10px;
+    }
+  }
+  .pokemonCard {
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .header {
+    font-size: 1rem;
+
+    & input[type="text"] {
+      font-size: 1rem;
+    }
+
+    & .searchButton {
+      font-size: 1rem;
+    }
+  }
+
+  .pokemonCard {
+    & .stats {
+      width: 90%;
+    }
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .header {
+    & label {
+      text-align: center;
+    }
+  }
 }
 </style>
